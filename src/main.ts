16d8330352221073962e3f5f6297a0ec70ee8266
DATBOI <alexsimons9999@gmail.com>
2021-05-23 11:52:39 -0500

    Can write markdown

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: '亚历二〇'
-	}
+		name: '亚历二〇',
+		description: `## Best Girl`,
+		}
 });
 
 export default app;
