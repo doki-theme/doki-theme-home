c625403ee020cdbd6450d17f426631dd5df5185b
DATBOI <alexsimons9999@gmail.com>
2021-05-26 08:01:36 -0500

    Working on displaying on the themes

--- src/Themes.svelte
+++ src/Themes.svelte
@@ -1,3 +1,26 @@
+<script>
+  import DokiThemeDefinitions from "./DokiThemeDefinitions";
 
+  const dokiThemes = Object.values(DokiThemeDefinitions).sort((a, b) =>
+    a.information.name.localeCompare(b.information.name)
+  );
+</script>
 
-<header>Themes</header>
+<div class="container">
+  {#each dokiThemes as dokiTheme}
+    <div class="theme">{dokiTheme.information.name}</div>
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: auto auto;
+  }
+
+  .theme {
+    padding: 1rem;
+    text-align: center;
+  }
+
+</style>
