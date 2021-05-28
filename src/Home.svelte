fb01cdaaf10cb5268608fdb070c56e0ff62d53c3
DATBOI <alexsimons9999@gmail.com>
2021-05-28 06:20:30 -0500

    Working on home

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
   // import marked from "marked";
 
-  
+  import DokiLogo from "./DokiLogo.svelte";
 </script>
 
-<header>Home</header>
+<div class="container">
+  <h1>The Doki Theme</h1>
+  <DokiLogo size="145" />
+  <p>
+    A large collection of multi-platform themes built with love.
+  </p>
+</div>
 
-  <!-- {@html marked(description)} -->
-
-
+<!-- {@html marked(description)} -->
 <style>
+  h1 {
+    font-size: 3rem;
+    font-weight: 300;
+  }
 
-
-  
+  .container {
+    max-width: 1200px;
+    margin: auto;
+    text-align: center;
+    padding: 1rem;
+  }
 </style>
