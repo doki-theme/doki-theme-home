ea3964415d3ddc1abde0a05c44f4b13fb990b3b7
Alex Simons <alexsimons9999@gmail.com>
2021-06-12 09:30:10 -0500

    Added v11 Themes (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,33 @@
 Changelog
 ---
 
+# 2.0.0
+
+## 5 New Themes
+
+From the Monogatari series:
+
+- Hanekawa Tsubasa (Dark)
+
+From the Yuru Camp series:
+
+- Shima Rin (Dark)
+
+From the Don't Toy With Me, Miss Nagatoro series:
+
+- Hayase Nagatoro (Dark)
+
+From the Kakegurui Series:
+
+- Jabami Yumeko (Dark)
+
+From the Future Diary Series
+
+- Gasai Yuno (Dark)
+
+![v16 Girls](https://doki.assets.unthrottled.io/misc/v16_girls.png)
+
+# 1.0.0
+
+- Initial Release!
+
--- src/JetBrainsProductUpdate.ts
+++ src/JetBrainsProductUpdate.ts
@@ -5,6 +5,10 @@ type Change = {
 };
 
 const changes: Change[] = [
+{
+    version: "16.0.0",
+    changes: `## 5 New Themes!\n---\n\nFrom the Monogatari series:\n\n- Hanekawa Tsubasa (Dark)\n\nFrom the Yuru Camp series:\n\n- Shima Rin (Dark)\n\nFrom the Don't Toy With Me, Miss Nagatoro series:\n\n- Hayase Nagatoro (Dark)\n\nFrom the Kakegurui Series:\n\n- Jabami Yumeko (Dark)\n\nFrom the Future Diary Series\n\n- Gasai Yuno (Dark)\n\n<div style="text-align: center">\n    <img alt="v16 girl" style="max-width: 700px; border-radius: 0.5rem" src="https://doki.assets.unthrottled.io/misc/v16_girls.png"/>\n</div>\n\n---`,
+  },
   {
     version: "15.3.0",
     changes: `- Changed update notification to be a browser window of the new [Doki Theme home page](https://doki-theme.unthrottled.io)`,
