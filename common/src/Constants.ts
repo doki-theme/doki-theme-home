17001a642635b54c1cb7cf0c2147c7fe69b0e76d
Alex Simons <alexsimons9999@gmail.com>
2023-02-27 18:46:17 -0600

    Added icons route.

--- common/src/Constants.ts
+++ common/src/Constants.ts
@@ -1,6 +1,7 @@
 export const navLinks = [
   { path: "/", label: "Home" },
   { path: "/themes", label: "Themes" },
+  { path: "/icons", label: "Icons" },
   { path: "/products", label: "Products" },
   { path: "/supporters", label: "Supporters" },
 ];
