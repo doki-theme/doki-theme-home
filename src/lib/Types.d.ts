e980b663435374b18e6f2ff1ab0abd1045a73bd2
Alex Simons <alexsimons9999@gmail.com>
2023-02-27 18:44:54 -0600

    Restored DokiTheme Typing.

--- src/lib/Types.d.ts
+++ src/lib/Types.d.ts
@@ -4,66 +4,74 @@ export interface DokiTheme {
   stickers: Stickers;
   backgrounds?: Backgrounds | null;
 }
+
 export interface DokiThemeLite {
   a: string;
   b?: string;
 }
+
 export interface Information {
   id: string;
   name: string;
   displayName: string;
   dark: boolean;
 }
-export interface Stickers1 {
-  default: string;
-}
-export interface Meta {
-  isVivid: string;
-}
+
 export interface Colors {
-  constantColor: string;
-  caretRow: string;
-  infoForeground: string;
-  baseIconColor: string;
-  selectionForeground: string;
-  headerColor: string;
-  baseBackground: string;
-  borderColor: string;
-  buttonColor: string;
-  selectionBackground: string;
-  buttonFont: string;
-  foregroundColor: string;
-  foregroundColorEditor: string;
-  highlightColor: string;
+  "terminal.ansiBlue": string;
+  "terminal.ansiCyan": string;
+  "terminal.ansiGreen": string;
+  "terminal.ansiMagenta": string;
+  "terminal.ansiYellow": string;
   accentColor: string;
   accentColorTransparent: string;
-  editorAccentColor: string;
   accentContrastColor?: string;
-  iconContrastColor?: string;
-  linkColor?: string;
-  accentContrastColor: string;
-  codeBlock: string;
-  textEditorBackground: string;
+  baseBackground: string;
+  baseIconColor: string;
+  borderColor: string;
+  buttonColor: string;
+  buttonFont: string;
+  caretRow: string;
   classNameColor: string;
+  codeBlock: string;
+  constantColor: string;
+  dokiLogoAccent: string;
+  dokiLogoAccentContrast: string;
+  editorAccentColor: string;
+  foregroundColor: string;
+  foregroundColorEditor: string;
+  headerColor: string;
+  highlightColor: string;
   htmlTagColor: string;
   iconBlendCompliment: string;
-  stringColor: string;
+  iconContrastColor?: string;
+  infoForeground: string;
   keyColor: string;
   keywordColor: string;
-  "terminal.ansiCyan": string;
-  "terminal.ansiYellow": string;
-  "terminal.ansiMagenta": string;
+  linkColor?: string;
+  selectionBackground: string;
+  selectionForeground: string;
+  stringColor: string
+  textEditorBackground: string;
 }
+
 export interface Stickers {
   default: DefaultOrSecondary;
+  secondary?: DefaultOrSecondary;
 }
+
 export interface DefaultOrSecondary {
   path: string;
   name: string;
 }
+
 export interface Backgrounds {
   default?: DefaultOrSecondary2 | null;
+  secondary?: DefaultOrSecondary2 | null;
 }
+
 export interface DefaultOrSecondary2 {
   anchor: string;
+  name: string;
+  opacity: number;
 }
