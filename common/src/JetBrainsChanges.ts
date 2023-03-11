be4dd9bed80c223c34e854d40e1994114dd60c38
Alex Simons <alexsimons9999@gmail.com>
2023-03-10 18:38:42 -0600

    Added JetBrains v88.5-1.9.6 release stuff

--- common/src/JetBrainsChanges.ts
+++ common/src/JetBrainsChanges.ts
@@ -7,6 +7,10 @@ export type Change = {
 
 export const changes: Change[] = [
   {
+    version: '88.5-1.9.6',
+    changes: `- Migrated to newer platform APIs.\n- Raised the lowest supported version to 2022.3`,
+  },
+  {
     version: '88.5-1.9.5',
     changes: `- Made Non-Functional & Non-User-facing code cleanup updates.`,
   },
